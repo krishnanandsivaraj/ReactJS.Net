a4edac0535d11507ffb37e3cdfcbdd364202ac8e
krishnanand.sivaraj@softcrylic.co.in <krishnanand.sivaraj@softcrylic.co.in>
2017-11-06 12:21:41 +0530

    react.js sample

---
+++ ReactJS.NET-getting-started-tutorial/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using ReactDemo.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace ReactDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly IList<CommentModel> _comments;
+
+        static HomeController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Author = "Nirmak Kumar",
+                    Text = "Hello React"
+                },
+                new CommentModel
+                {
+                    Author = "Krishnanand Sivaraj",
+                    Text = "Hi Guys!!"
+                },
+                new CommentModel
+                {
+                    Author = "Ramachandran Murugan",
+                    Text = "I am also working!!"
+                },
+            };
+        }
+
+        [HttpPost]
+        public ActionResult AddComment(CommentModel comment)
+        {
+            _comments.Add(comment);
+            return Content("Success :)");
+        }
+
+        public ActionResult Comments()
+        {
+            return Json(_comments, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Index()
+        {
+            return View(_comments);
+        }
+
+    }
+}
